57d7aa54a64458da7a84aef7b101cf7d8a413106
Hyunmo <yhmgood0130@gmail.com>
2017-07-27 15:21:52 -0600

    All functions added

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
               <img id="title-image" src=${data[i].url} class="img-responsive title-image" alt="Generic placeholder thumbnail">
               <h2 id="title-name">${data[i].title}</h2>
               <p id="title-price"> Credit: $ ${data[i].price} </p>
-              <button type="button" id="cart" class="btn btn-default" aria-label="Left Align">
+              <button type="button" id="addCart" class="btn btn-default" aria-label="Left Align">
                 <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true">&nbsp;</span>Add Cart
               </button>
             </div>`)
@@ -58,7 +58,7 @@ $(document).ready(function() {
         <img id="title-image" src=${data[i].url} class="img-responsive title-image" alt="Generic placeholder thumbnail">
         <h2 id="title-name">${data[i].title}</h2>
         <p id="title-price"> Credit: $ ${data[i].price} </p>
-        <button type="button" id="cart" class="btn btn-default" aria-label="Left Align">
+        <button type="button" id="addCart" class="btn btn-default" aria-label="Left Align">
           <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true">&nbsp;</span>Add Cart
         </button>
       </div>`)
@@ -74,7 +74,7 @@ $(document).ready(function() {
           <img id="title-image" src=${data[i].url} class="img-responsive title-image" alt="Generic placeholder thumbnail">
           <h2 id="title-name">${data[i].title}</h2>
           <p id="title-price"> Credit: $ ${data[i].price} </p>
-          <button type="button" id="cart" class="btn btn-default" aria-label="Left Align">
+          <button type="button" id="addCart" class="btn btn-default" aria-label="Left Align">
             <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true">&nbsp;</span>Add Cart
           </button>
         </div>`)
@@ -90,7 +90,7 @@ $(document).ready(function() {
           <img id="title-image" src=${data[i].url} class="img-responsive title-image" alt="Generic placeholder thumbnail">
           <h2 id="title-name">${data[i].title}</h2>
           <p id="title-price"> Credit: $ ${data[i].price} </p>
-          <button type="button" id="cart" class="btn btn-default" aria-label="Left Align">
+          <button type="button" id="addCart" class="btn btn-default" aria-label="Left Align">
             <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true">&nbsp;</span>Add Cart
           </button>
         </div>`)
@@ -107,7 +107,7 @@ $(document).ready(function() {
           <img id="title-image" src=${data[i].url} class="img-responsive title-image" alt="Generic placeholder thumbnail">
           <h2 id="title-name">${data[i].title}</h2>
           <p id="title-price"> Credit: $ ${data[i].price} </p>
-          <button type="button" id="cart" class="btn btn-default" aria-label="Left Align">
+          <button type="button" id="addCart" class="btn btn-default" aria-label="Left Align">
             <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true">&nbsp;</span>Add Cart
           </button>
         </div>`)
@@ -115,20 +115,148 @@ $(document).ready(function() {
     })
   })
 
-  $("#game-list").on('click', '#cart',function(){
-    var numberPattern = /[+-]?\d+(\.\d+)?/g
-    var prices = $(this).prevAll('#title-price').text().match(numberPattern)
-
-    $.post("https://afternoon-bastion-62299.herokuapp.com/product",
-    {
-      title:$(this).prevAll('#title-name').text(),
-      platform:$(this).parent().before().attr('id'),
-      price:parseFloat(prices),
-      url:$(this).prevAll('#title-image').attr('src')
-    },
-    function(data,status){
-      alert("Data: " + data + "\nStatus: " + status);
+  $('#SWITCH').click(function(){
+    let itemName;
+    $.get('https://afternoon-bastion-62299.herokuapp.com/product/switch' , function(data){
+      $("#game-list").empty();
+      for (var i = 0; i < data.length; i++) {
+        $("#game-list").append(`<div id="SWITCH" class="col-xs-6 col-sm-3 row-height">
+          <img id="title-image" src=${data[i].url} class="img-responsive title-image" alt="Generic placeholder thumbnail">
+          <h2 id="title-name">${data[i].title}</h2>
+          <p id="title-price"> Credit: $ ${data[i].price} </p>
+          <button type="button" id="addCart" class="btn btn-default" aria-label="Left Align">
+            <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true">&nbsp;</span>Add Cart
+          </button>
+        </div>`)
+      }
     })
   })
 
+
+  $('#cart').click(function(){
+    let consoleType;
+
+    cartDisplay();
+
+
+
+  })
+
+  $(document).on('change','#counter',function() {
+    var quantity = parseInt($(this).children("option:selected").text());
+    console.log(quantity);
+    let id = $(this).closest("#game-list").children().attr("id");
+
+    $.ajax({
+      url:"http://localhost:8080/product/" + id,
+      type:"PUT",
+      data:{quantity},
+      success: function(data){
+        alert(Data + "Achtung!!!")
+      }
+
+
+    })
+  })
+
+  $("#game-list").on('click', '#addCart',function(){
+    var numberPattern = /[+-]?\d+(\.\d+)?/g
+    let number;
+    let exist = false;
+    let title = $(this).prevAll('#title-name').text();
+    let platform =$(this).parent().before().attr('id');
+    let price = parseFloat(prices);
+    let url = $(this).prevAll('#title-image').attr('src');
+    var prices = $(this).prevAll('#title-price').text().match(numberPattern)
+
+    $.get("http://localhost:8080/product/cart",function(data){
+      for (var i = 0; i < data.length; i++) {
+        let id = data[i].id;
+
+         if(data[i].quantity > 0 && data[i].title == title && data[i].platform == platform){
+          number = data[i].quantity + 1;
+          console.log("SECOND", id);
+          $.ajax({
+            url:"http://localhost:8080/product/" + id,
+            type:"PUT",
+            data:{
+              quantity:number
+            },
+            success: function(data){
+              alert(Data + "Achtung!!!")
+            }
+           })
+           exist = true;
+          }
+      }
+
+      if(!exist){
+        $.post("http://localhost:8080/product",
+        {
+          title:title,
+          platform:platform,
+          price:price,
+          url:url,
+          quantity:1
+        },
+        function(data,status){
+          alert("Data: " + data + "\nStatus: " + status);
+        })
+      }
+
+    })
+  })
+
+  $("#game-list").on('click', '#deleteItem',function(){
+
+    $.ajax({
+      url:"http://localhost:8080/product/" + $(this).parent().parent().before().attr('id'),
+      type: 'DELETE',
+      success: function(result)
+      {
+        console.log("YES!!!");
+      }
+    })
+
+    window.setTimeout(function(){cartDisplay();},1000);
+
+  })
+
 })
+
+function cartDisplay(){
+$.get('http://localhost:8080/product/cart', function(data){
+  $("#game-list").empty();
+for (var i = 0; i < data.length; i++) {
+  var dynamic = "";
+  for (var j = 1; j < 5;j++){
+    if(data[i].quantity == j){
+      dynamic += `<option selected>${j}</option>`
+    }
+    else{
+      dynamic += `<option>${j}</option>`
+    }
+  }
+
+  $("#game-list").append(`<div id="${data[i].id}" class="col-xs-6 col-sm-3 cart-box">
+    <img id="title-image" src=${data[i].url} class="img-responsive title-image" alt="Generic placeholder thumbnail">
+    <h2 id="title-name">${data[i].title}</h2>
+    <p id="title-price"> Credit: $ ${data[i].price} </p
+    <div class="input-container row">
+     <div class="input-box">
+      <button type="button" id="deleteItem" class="btn btn-default col-sm-6 col-xs-6" aria-label="Left Align">
+        <span class="glyphicon glyphicon-remove" aria-hidden="true">&nbsp;</span>Delete
+      </button>
+      <form>
+        <div class="form-group col-sm-4 col-xs-4">
+          <select class="form-control" id="counter">
+          ${dynamic}
+          </select>
+        </div>
+      </form>
+      </div>
+    </div>
+  </div>`)
+  }
+})
+}
